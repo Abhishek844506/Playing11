370e2c4123c4ac7193eae8c9aa374636766707ad
Abhishek844506 <abhishekkrnirankari2001@gmail.com>
2026-03-27 01:14:00 +0530

    Changed api key and hide it

--- api/groq.js
+++ api/groq.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
-  // Allow CORS
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'POST')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
   if (req.method === 'OPTIONS') {
